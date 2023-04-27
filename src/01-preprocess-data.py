b2e86612fbe394bf4780ba0d10788827b0dbecb8
Ethan Allavarpu <63200263+ethan-allavarpu@users.noreply.github.com>
2023-04-27 15:34:52 -0700

    update to 383 videos

--- src/01-preprocess-data.py
+++ src/01-preprocess-data.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import shutil
 
+
 def get_resp_bucket(scaled_response):
     if scaled_response == 0:
         return 0
@@ -10,20 +11,17 @@ def get_resp_bucket(scaled_response):
     else:
         return 2
 
+
 if __name__ == "__main__":
     pose_data = pd.read_csv("data/raw/dataClean_text.csv")
     # Extract valid IDs
-    valid_ids = pose_data.subjectid.to_list()
-    with open("data/processed/valid-video-ids.txt", "w") as f:
-        f.writelines([id + "\n" for id in valid_ids])
-    
-    # Filter and process survey data accordingly
-    col_names = pd.read_csv("data/raw/dataSurvey.csv", nrows=1).columns
-    survey = pd.read_csv("data/raw/dataSurvey.csv", header=1, names=col_names)
-    valid_survey = survey[[id in valid_ids for id in survey.subjectid]]
-    # Make response variable
-    valid_survey["y_fall_risk"] = valid_survey.falling_1.apply(get_resp_bucket)
-    valid_survey.to_csv("data/processed/survey-data-processed.csv", index=False)
+    valid_ids = set(pose_data.subjectid.to_list())
+    video_files = os.listdir("data/raw/videos/")
+    video_id = set([f.split(".mp4")[0] for f in os.listdir("data/raw/videos/")])
+    kept_ids = list(valid_ids.intersection(video_id))
+
+    with open("data/processed/kept-video-ids.txt", "w") as f:
+        f.writelines([id + "\n" for id in kept_ids])
 
     # Extract valid videos
     out_dir = "data/processed/valid-videos/"
@@ -33,10 +31,27 @@ if __name__ == "__main__":
     # If directory not present, create it
     else:
         os.makedirs(out_dir)
-    video_files = os.listdir("data/raw/videos/")
-    valid_video_files = [path for path in os.listdir("data/raw/videos/")
-                         if path.split(".mp4")[0] in valid_ids]
+    valid_video_files = [
+        path
+        for path in os.listdir("data/raw/videos/")
+        if path.split(".mp4")[0] in kept_ids
+    ]
     for file_path in valid_video_files:
-        shutil.copy2(os.path.join("data/raw/videos/", file_path),
-                     os.path.join(out_dir, file_path))
-    print(f"{len(valid_video_files)} valid video files")
+        shutil.copy2(
+            os.path.join("data/raw/videos/", file_path),
+            os.path.join(out_dir, file_path),
+        )
+
+    # Filter and process survey data accordingly
+    col_names = pd.read_csv("data/raw/dataSurvey.csv", nrows=1).columns
+    survey = pd.read_csv("data/raw/dataSurvey.csv", header=1, names=col_names)
+    valid_survey = survey[[id in kept_ids for id in survey.subjectid]]
+    # Make response variable
+    # Remove copy warning
+    pd.set_option("mode.chained_assignment", None)
+    valid_survey["y_fall_risk"] = valid_survey.falling_1.apply(get_resp_bucket)
+    # Reinstate copy warning
+    pd.set_option("mode.chained_assignment", "warn")
+    valid_survey.to_csv("data/processed/survey-data-processed.csv", index=False)
+    print(f"{len(kept_ids)} unique observations in processed data")
+    print(f"All processed data in data/processed/")
