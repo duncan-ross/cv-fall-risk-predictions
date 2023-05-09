589b4774ac74ba9f6a0953c249ef9e6b1a7a551e
Ethan Allavarpu <63200263+ethan-allavarpu@users.noreply.github.com>
2023-05-08 17:56:43 -0700

    add weighted cross-entropy

--- scripts/evaluation-results.sh
+++ scripts/evaluation-results.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 python src/evaluation.py --predictions_file # Insert predictions file here
+python src/evaluation.py --predictions_file --loss "weighted_ce" --weight_file
--- src/evaluation.py
+++ src/evaluation.py
@@ -7,6 +7,8 @@ from torch.nn.functional import cross_entropy
 
 argp = argparse.ArgumentParser()
 argp.add_argument("--predictions_file", type=str, required=True)
+argp.add_argument("--loss", type=str, default="ce", required=False)
+argp.add_argument("--weight_file", type=str, default=None, required=False)
 
 if __name__ == "__main__":
     args = argp.parse_args()
@@ -14,11 +16,27 @@ if __name__ == "__main__":
     probs = np.array(results.iloc[:, 2:5]) # Middle three columns
     y_true = np.array(results.iloc[:, -1]).reshape(-1)
     y_pred = np.argmax(probs, axis=1)
+    conf_mat = confusion_matrix(y_true=y_true, y_pred=y_pred)
+    if argp.loss == "ce":
+        weights = np.ones(probs.shape[1])
+        acc = accuracy_score(y_true, y_pred)
+    elif argp.loss == "weighted_ce":
+        if argp.weight_file is None:
+            raise ValueError("loss of weighted_ce, but no weight file provided")
+        weights = np.loadtxt(argp.weight_file)
+        num = (np.diag(conf_mat) * weights).sum()
+        den = (np.bincount(y_true) * weights).sum()
+        acc = num / den
+    else:
+        e = "loss argument must be one of 'ce' (cross entropy) "
+        e += "or 'weighted_ce (weighted cross entropy)"
+        raise ValueError(e)
     ce = cross_entropy(
         input=torch.Tensor(y_pred),
-        target=torch.Tensor(y_true).type(torch.LongTensor)
+        target=torch.Tensor(y_true).type(torch.LongTensor),
+        weight=weights
     )
     print(f"Cross-Entropy Loss on Test Set: {ce}")
-    print(f"Accuracy on Test Set: {accuracy_score(y_true, y_pred)}")
-    print(f"Confusion Matrix of Results:")
-    print(confusion_matrix(y_true=y_true, y_pred=y_pred))
+    print(f"Accuracy on Test Set: {acc}")
+    print(f"Confusion Matrix of Results:\n{conf_mat}")
+
