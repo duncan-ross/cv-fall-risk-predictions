a05f800c518fd403109059cec50e9f51fb23558f
Ethan Allavarpu <63200263+ethan-allavarpu@users.noreply.github.com>
2023-05-25 08:38:10 -0700

    survey dataloader

--- src/data_loading/dataloaders.py
+++ src/data_loading/dataloaders.py
@@ -9,7 +9,7 @@ import warnings
 import numpy as np
 import torchvision
 
-from data_loading.datasets import VideoLabelDataset, MotionCaptureDataset
+from data_loading.datasets import VideoLabelDataset, MotionCaptureDataset, SurveyDataset
 
 
 def get_vid_data_loaders(
@@ -97,6 +97,43 @@ def get_mc_data_loaders(
             shuffle=True,
             num_workers=num_workers,
             collate_fn=collate_fn,
+        )
+
+    return ds_dict["train"], ds_dict["val"], ds_dict["test"]
+
+def get_survey_data_loaders(
+    batch_size: int = 32,
+    val_batch_size: int = 16,
+    test_batch_size: int = 16,
+    transforms: Any = None,
+    labels: List = ["y_fall_risk"],
+    num_workers: int = 0,
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    ds_dict = {}
+    for ds in ["train", "val", "test"]:
+        # just do the resize for the val/test set
+        if len(transforms.transforms) > 0:
+            transforms = (
+                transforms
+                if ds == "train"
+                else torchvision.transforms.Compose([transforms.transforms[0]])
+            )
+        else:
+            transforms = transforms
+
+        dataset = SurveyDataset(
+            tabular_csv=f"data/processed/{ds}-survey-data.csv",
+            labels=labels,
+        )
+        ds_dict[ds] = DataLoader(
+            dataset,
+            batch_size=batch_size
+            if ds == "train"
+            else val_batch_size
+            if ds == "val"
+            else test_batch_size,
+            shuffle=True,
+            num_workers=num_workers,
         )
 
     return ds_dict["train"], ds_dict["val"], ds_dict["test"]
