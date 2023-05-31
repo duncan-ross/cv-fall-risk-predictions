47414fd7cb9a16522bc9e94a3adac3737f5e0a2b
Ethan Allavarpu <63200263+ethan-allavarpu@users.noreply.github.com>
2023-05-31 12:04:38 -0700

    fix eval

--- scripts/evaluation-results.sh
+++ scripts/evaluation-results.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-python src/evaluation.py --predictions_file # Insert predictions file here
-
 python3 src/evaluation.py \
     --predictions_file "predictions/logistic-predictions.csv" \
     --loss "weighted_ce"
 
 python3 src/evaluation.py \
-    --predictions_file "predictions/logistic-predictions-training.csv" \
+    --predictions_file "predictions/survey-nn-predictions.csv" \
     --loss "weighted_ce"
 
 python3 src/evaluation.py \
-    --predictions_file "predictions/survey-nn-predictions.csv" \
+    --predictions_file "predictions/transformer/transformer-predictions.csv" \
+    --loss "weighted_ce"
+
+python3 src/evaluation.py \
+    --predictions_file "predictions/fusion_mc_preds.csv" \
     --loss "weighted_ce"
